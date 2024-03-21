ecb9b6581b423a9d56c58b2306bac9112ecfc7af
Maksim Milyutin <mmxckk@gmail.com>
2024-03-21 11:18:09 +0100

    Is development

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,11 +5,7 @@ export const UI_VERSION = "1.4";
 export const IS_TOUCH = "ontouchstart" in window;
 
 export function isDevelopment() {
-  const isProductionPreview = Boolean(localStorage.getItem(PRODUCTION_PREVIEW_KEY));
-
-  return (
-    !window.location.host?.includes("gmx.io") && !window.location.host?.includes("ipfs.io") && !isProductionPreview
-  );
+  return true;
 }
 
 export function isLocal() {
